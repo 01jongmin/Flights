6e0aad448e92089f35f30504182125ed47e40870
Jong Min Choi <01jongminchoi@gmail.com>
2021-11-23 12:53:48 -0500

    changed fetch url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  React.useEffect(async () => {
+    async function fetchMyAPI() {
+      var res = await fetch(`https://flights.onrender.com/world`, {
+        method: "GET",
+      });
+      console.log(res);
+    }
+
+    fetchMyAPI();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-        </p>
+        <p>Edit</p>
         <a
           className="App-link"
           href="https://reactjs.org"
