5bfabb60de5ac75f1396890eacb913d8f4d76790
Jong Min Choi <01jongminchoi@gmail.com>
2021-12-11 17:21:17 -0500

    changed cors

--- backend/src/airports.rs
+++ backend/src/airports.rs
@@ -4,7 +4,9 @@ use crate::schema::Airports::dsl::Airports;
 use rocket::serde::json::Json;
 use rocket::http::Status;
 use rocket_okapi::openapi;
-use diesel::{prelude::*};
+use diesel::{prelude::*, sql_types::*};
+use serde::{Serialize};
+use schemars::JsonSchema;
 
 #[openapi(tag = "Airports")]
 #[get("/?<page_size>&<page>")]
@@ -22,4 +24,14 @@ pub async fn get_all_airports(conn: MyDatabase, page_size: Option<u32>, page: Op
         Ok(all_airports) => Ok(Json(all_airports)),
         Err(_) => Err(Status::BadRequest),
     }
+}
+
+#[derive(Serialize, QueryableByName, JsonSchema, Debug)]
+struct AirportDistance {
+    #[sql_type="Integer"]
+    pub target_id: i32,
+    #[sql_type="Double"]
+    pub distance: f64,
+    #[sql_type="Double"]
+    pub hours: f64,
 }
