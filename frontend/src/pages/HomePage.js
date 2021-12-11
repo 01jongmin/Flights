d86f7ec7bb11cd8ed2ff41c6fa13ccb330923b85
Daniel Ng <9526186+DanielYKNg@users.noreply.github.com>
2021-12-11 14:20:30 -0500

    Revert to HW2M2 Files
    Change all, add packages to package.json, remove all API references .

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+  Table,
+  Pagination,
+  Select
+} from 'antd'
+
+import MenuBar from '../components/MenuBar';
+import { getAllMatches, getAllPlayers } from '../fetcher'
+const { Column, ColumnGroup } = Table;
+const { Option } = Select;
+
+class HomePage extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    // this.state = {
+    //   matchesResults: [],
+    //   matchesPageNumber: 1,
+    //   matchesPageSize: 10,
+    //   playersResults: [],
+    //   pagination: null  
+    // }
+
+    // this.leagueOnChange = this.leagueOnChange.bind(this)
+    // this.goToMatch = this.goToMatch.bind(this)
+  }
+
+
+  // goToMatch(matchId) {
+  //   window.location = `/matches?id=${matchId}`
+  // }
+
+  leagueOnChange(value) {
+    // THINK? TASK 2: this value should be used as a parameter to call getAllMatches in fetcher.js with the parameters page and pageSize set to null
+    // then, matchesResults in state should be set to the results returned - see a similar function call in componentDidMount()
+    
+    // getAllMatches(null, null, value).then(res => this.setState({
+    //   matchesResults: res.results
+    // }))
+
+  }
+
+  componentDidMount() {
+    // getAllMatches(null, null, 'D1').then(res => {
+    //   this.setState({ matchesResults: res.results })
+    // })
+
+    // getAllPlayers().then(res => {
+    //   console.log(res.results);
+    //         this.setState({
+    //           playersResults: res.results
+    //         })
+
+      //  TASK 1: set the correct state attribute to res.results
+    // }
+    
+
+ 
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <MenuBar />
+        <div style={{ width: '70vw', margin: '0 auto', marginTop: '5vh' }}>
+                  <h4> Welcome </h4>
+
+
+
+        </div>
+
+
+      </div>
+    )
+  }
+
+}
+
+export default HomePage
+
