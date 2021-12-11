4d2f7aa9f6f88a35d26bb17f1b1c4769268d2125
Jong Min Choi <01jongminchoi@gmail.com>
2021-12-11 15:15:23 -0500

    changed menu

--- frontend/src/components/MenuBar.js
+++ frontend/src/components/MenuBar.js
@@ -1,37 +1,31 @@
-import React from 'react';
-import {
-    Navbar,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink
-  } from "shards-react";
+import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "shards-react";
 
 class MenuBar extends React.Component {
-    render() {
-        return(
-            <Navbar type="dark" theme="primary" expand="md">
+  render() {
+    return (
+      <Navbar type="dark" theme="primary" expand="md">
         <NavbarBrand href="/">CIS 550 FIFA</NavbarBrand>
-          <Nav navbar>
+        <Nav navbar>
           <NavItem>
-              <NavLink active href="/">
-                Home
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink active href="/players">
-                Players
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink active  href="/matches" >
-                Matches
-              </NavLink>
-            </NavItem>
-          </Nav>
+            <NavLink active href="/Flights/">
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active href="/Flights/players">
+              Players
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active href="/Flights/matches">
+              Matches
+            </NavLink>
+          </NavItem>
+        </Nav>
       </Navbar>
-        )
-    }
+    );
+  }
 }
 
-export default MenuBar
+export default MenuBar;
