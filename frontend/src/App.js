37abe8668e3ed0068f8f3b886bc332e44929d994
Arushi Aggarwal <arushi.aggarwal@hotmail.com>
2021-12-12 17:54:01 -0500

    fixed alliance subpage routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import PlayerPage from "./pages/PlayersPage.js";
 import MatchPage from "./pages/MatchesPage.js";
 import Home from "./pages/HomePage.js";
 import AlliancesPage from "./pages/AlliancesPage";
+import AlliancePage from "./pages/AlliancePage";
 import AirportsPage from "./pages/AirportsPage";
 
 class App extends React.Component {
@@ -20,6 +21,9 @@ class App extends React.Component {
           </Route>
           <Route exact path="/alliances">
             <AlliancesPage />
+          </Route>
+          <Route exact path="/alliance">
+            <AlliancePage />
           </Route>
           <Route exact path="/airports">
             <AirportsPage />
