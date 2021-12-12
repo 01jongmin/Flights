4737b880746b23455a596d5cc66e460877f20744
Jong Min Choi <01jongminchoi@gmail.com>
2021-12-11 19:58:10 -0500

    cors fixed?

--- backend/src/cors.rs
+++ backend/src/cors.rs
@@ -2,7 +2,7 @@ use rocket::http::Method;
 use rocket_cors::{Cors, AllowedHeaders, AllowedOrigins};
 
 pub fn get_cors_option() -> Cors {
-    let allowed_origins = AllowedOrigins::some_regex(&["https://01jongmin.github.io/Flights/*", "http::localhost:*"]);
+    let allowed_origins = AllowedOrigins::some_regex(&["http://rocket-env.eba-jzvxeuty.us-east-2.elasticbeanstalk.com/*", "http://localhost:*"]);
 
     rocket_cors::CorsOptions {
         allowed_origins,
