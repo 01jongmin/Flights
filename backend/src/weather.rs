249e1767f7f1a60605b4b23ca89f53a251acf860
Jong Min Choi <01jongminchoi@gmail.com>
2021-12-14 17:32:20 -0500

    fixed average temp

--- backend/src/weather.rs
+++ backend/src/weather.rs
@@ -16,41 +16,41 @@ pub struct AverageYearTempResponse {
 
 #[openapi(tag = "Weather")]
 #[get("/average_temp?<city>&<start_m>&<start_d>&<end_m>&<end_d>")]
-pub async fn get_average_temp(conn: MyDatabase, city: String, start_m: i32, start_d: i32, end_m: i32, end_d: i32) -> Result<Json<Vec<AverageYearTempResponse>>, Status> {
+pub async fn get_average_temp(conn: MyDatabase, city: i32, start_m: i32, start_d: i32, end_m: i32, end_d: i32) -> Result<Json<Vec<AverageYearTempResponse>>, Status> {
     let average_temp = conn.run(move |c| {
         let query = format!(
             "WITH wID AS (SELECT weatherID FROM (
-                          SELECT airID,
-                                 weatherID,
-                                 6371 * 2 * ATAN2(SQRT(SIN(dLat / 2) * SIN(dLat / 2) +
-                                                       COS(rLat1) * COS(rLat2) * SIN(dLon / 2) * SIN(dLon / 2)), SQRT(
-                                                              1 - (SIN(dLat / 2) * SIN(dLat / 2) +
-                                                                   COS(rLat1) * COS(rLat2) * SIN(dLon / 2) * SIN(dLon / 2))))
-                                     AS distance
-                          FROM (SELECT airID,
-                                       weatherID,
-                                       (lat2 - lat1) * PI() / 180 AS dLat,
-                                       (lon2 - lon1) * PI() / 180 as dLon,
-                                       lat1 * PI() / 180          AS rLat1,
-                                       lat2 * PI() / 180          AS rLat2
-                                FROM (SELECT a.id   AS airID,
-                                             ws.id  AS weatherID,
-                                             a.lat  as lat1,
-                                             a.lon  as lon1,
-                                             ws.lat as lat2,
-                                             ws.lon as lon2
-                                      FROM (SELECT * FROM Airports WHERE id = {} LIMIT 1) a
-                                               JOIN WeatherStation ws
-                                      WHERE a.country = ws.country) t) d
-                          ORDER BY distance
-                      ) d LIMIT 1)
-            SELECT YEAR(date), AVG(temp) FROM Weather
-            WHERE Weather.station_id IN (SELECT * FROM wID) AND
-                  MONTH(date) >= {} AND
-                  MONTH(date) <= {} AND
-                  DAY(date) >= {} AND
-                  DAY(date) <= {}
-            GROUP BY YEAR(date)",
+            SELECT airID,
+weatherID,
+6371 * 2 * ATAN2(SQRT(SIN(dLat / 2) * SIN(dLat / 2) +
+COS(rLat1) * COS(rLat2) * SIN(dLon / 2) * SIN(dLon / 2)), SQRT(
+1 - (SIN(dLat / 2) * SIN(dLat / 2) +
+COS(rLat1) * COS(rLat2) * SIN(dLon / 2) * SIN(dLon / 2))))
+AS distance
+FROM (SELECT airID,
+weatherID,
+(lat2 - lat1) * PI() / 180 AS dLat,
+(lon2 - lon1) * PI() / 180 as dLon,
+lat1 * PI() / 180 AS rLat1,
+lat2 * PI() / 180 AS rLat2
+FROM (SELECT a.id AS airID,
+ws.id AS weatherID,
+a.lat as lat1,
+a.lon as lon1,
+ws.lat as lat2,
+ws.lon as lon2
+FROM (SELECT * FROM Airports WHERE id = {} LIMIT 1) a
+JOIN WeatherStation ws
+WHERE a.country = ws.country) t) d
+ORDER BY distance
+) d LIMIT 1)
+SELECT YEAR(date) as year, AVG(temp) as average_temp FROM Weather
+WHERE Weather.station_id IN (SELECT * FROM wID) AND
+    MONTH(date) >= {} AND
+    MONTH(date) <= {} AND
+    DAY(date) >= {} AND
+    DAY(date) <= {}
+GROUP BY YEAR(date)",
             city,
             start_m,
             end_m,
@@ -62,7 +62,10 @@ pub async fn get_average_temp(conn: MyDatabase, city: String, start_m: i32, star
 
     match average_temp {
         Ok(average_temp) => Ok(Json(average_temp)),
-        Err(_) => Err(Status::BadRequest),
+        Err(e) => {
+            print!("{}", e);
+            Err(Status::BadRequest)
+        }
     }
 }
 
